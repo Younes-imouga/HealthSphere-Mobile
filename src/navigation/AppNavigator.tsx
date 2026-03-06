228a1cbb38fb2035130cb0247c0e2b17de0835e9
Asfor Dounia <asfordounia@gmail.com>
2026-03-06 07:41:54 +0000

    HEAL-1 Navigation Avancée

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { COLORS } from "../theme/constants";
 
 import AddWorkoutScreen from "../screens/AddWorkoutScreen";
-import HomeScreen from "../screens/HomeScreen";
 import WorkoutDetailsScreen from "../screens/WorkoutDetailsScreen";
+import DrawerNavigator from "./DrawerNavigator";
 
 export type RootStackParamList = {
-  Home: undefined;
+  DrawerNav: undefined;
   AddWorkout: undefined;
   WorkoutDetails: { id: string };
 };
@@ -17,18 +17,15 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function AppNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Home"
       screenOptions={{
         headerShown: false,
-        // 'slide_from_right' animation provides a much smoother "slide over" effect where
-        // the previous screen stays visible, avoiding black/white flashes.
         animation: "slide_from_right",
         contentStyle: {
           backgroundColor: COLORS.background.primary,
         },
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="DrawerNav" component={DrawerNavigator} />
       <Stack.Screen name="AddWorkout" component={AddWorkoutScreen} />
       <Stack.Screen name="WorkoutDetails" component={WorkoutDetailsScreen} />
     </Stack.Navigator>
